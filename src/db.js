0af2026c728ca3a76cd3c2259ed9fe56fa8fb182
Charuhas Reddy <charuhasreddybalam@gmail.com>
2023-10-09 16:50:03 +0530

    fixed db.js

--- src/db.js
+++ src/db.js
@@ -2,9 +2,24 @@ import fs from "node:fs/promises";
 
 const DB_PATH = new URL("../db.json", import.meta.url);
 
+// export const readDB = async () => {
+//   const db = await fs.readFile(DB_PATH, "utf-8");
+//   return JSON.parse(db);
+// };
+
 export const readDB = async () => {
-  const db = await fs.readFile(DB_PATH, "utf-8");
-  return JSON.parse(db);
+  try {
+    const db = await fs.readFile(DB_PATH, "utf-8");
+    return JSON.parse(db);
+  } catch (err) {
+    // If file not found, create it
+    if (err.code === "ENOENT") {
+      await fs.writeFile(DB_PATH, JSON.stringify({ notes: [] }, null, 2));
+      return { notes: [] };
+    }
+    // If other error, throw it
+    throw err;
+  }
 };
 
 export const saveDB = async (db) => {
